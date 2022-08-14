1c72e827b41442dcdb02389e9f39d357d7bc7500
Xinlong Chen <xl_chen@zju.edu.cn>
2022-08-14 18:20:00 +0600

    fix: commit lab2-part1

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -6,7 +6,9 @@
 // automated checks run by `make check_lab2`.
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
+
+#define UINT32_MOD (1ul << 32)
 
 using namespace std;
 
@@ -14,8 +16,8 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint32_t num = (n << 32) >> 32;
+    return isn + num;
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +31,15 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t abs = n - isn;
+    if (checkpoint <= abs) {
+        return abs;
+    } else {
+        uint64_t quotient = (checkpoint - abs) >> 32;
+        uint64_t remainder = ((checkpoint - abs) << 32) >> 32;
+        if (remainder < UINT32_MOD / 2) {
+            return abs + quotient * UINT32_MOD;
+        }
+        return abs + (quotient + 1) * UINT32_MOD;
+    }
 }
